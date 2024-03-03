43ccd3f95c887e10c72a5e98e3334d54851ec61f
Kvazim <mesvladimir@gmail.com>
2024-03-03 19:21:01 +0300

    fix: рефакторинг карты

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,6 +1,6 @@
-import leaflet from 'leaflet';
+import {Icon, Marker, layerGroup} from 'leaflet';
 import { useEffect, useRef } from 'react';
-import { Offers } from '../../types/offers';
+import { Offer, Offers } from '../../types/offers';
 import useMap from '../../hooks/use-map';
 import 'leaflet/dist/leaflet.css';
 import { URL_MARKER_CURRENT, URL_MARKER_DEFAULT } from '../../const';
@@ -11,30 +11,30 @@ type MapProps = {
   activeOfferId?: string | null;
 }
 
+const defaultCustomIcon = new Icon({
+  iconUrl: URL_MARKER_DEFAULT,
+  iconSize: [28, 40],
+  iconAnchor: [14, 40]
+});
+
+const currentCustomIcon = new Icon({
+  iconUrl: URL_MARKER_CURRENT,
+  iconSize: [28, 40],
+  iconAnchor: [14, 40]
+});
+
 function Map({className, offers, activeOfferId}: MapProps): JSX.Element {
   const mapRef = useRef(null);
   const cityLocation = offers[0].city.location;
   const map = useMap(mapRef, cityLocation);
 
-  const defaultCustomIcon = leaflet.icon({
-    iconUrl: URL_MARKER_DEFAULT,
-    iconSize: [28, 40],
-    iconAnchor: [14, 40]
-  });
-
-  const currentCustomIcon = leaflet.icon({
-    iconUrl: URL_MARKER_CURRENT,
-    iconSize: [28, 40],
-    iconAnchor: [14, 40]
-  });
-
   useEffect(() => {
     if (map) {
       map.flyTo([cityLocation.latitude, cityLocation.longitude], cityLocation.zoom);
-      const markerGroup = leaflet.layerGroup().addTo(map);
+      const markerGroup = layerGroup().addTo(map);
 
-      offers.forEach((offer) => {
-        const marker = leaflet.marker({
+      const addMarker = (offer: Offer) => {
+        const marker = new Marker({
           lat: offer.location.latitude,
           lng: offer.location.longitude
         });
@@ -46,13 +46,15 @@ function Map({className, offers, activeOfferId}: MapProps): JSX.Element {
               : defaultCustomIcon
           )
           .addTo(markerGroup);
-      });
+      };
+
+      offers.forEach((offer) => addMarker(offer));
 
       return () => {
         map.removeLayer(markerGroup);
       };
     }
-  }, [cityLocation, currentCustomIcon, activeOfferId, defaultCustomIcon, map, offers]);
+  }, [activeOfferId, cityLocation, map, offers]);
 
   return (
     <section ref={mapRef} className={`${className}__map map`}></section>
