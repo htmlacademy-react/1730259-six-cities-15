1eb2fbe4b7b78233b2298d8c573b24a24d122d04
Владимир <mesvladimir@gmail.com>
2024-03-18 15:55:37 +0300

    fix: ревью

--- src/components/location-link/location-link.tsx
+++ src/components/location-link/location-link.tsx
@@ -1,7 +1,7 @@
 import { Link, useSearchParams } from 'react-router-dom';
 import { capitalize } from '../../utils/utils';
 import cn from 'classnames';
-import { AppRoute, CITY, Cities, DEFAULT_SORT, SORT_TYPE } from '../../const';
+import { AppRoute, CITY, Cities, DEFAULT_SORT, SORT_TYPE, SortType } from '../../const';
 import { useMemo } from 'react';
 
 type LocationLinkProps = {
@@ -11,14 +11,15 @@ type LocationLinkProps = {
 
 function LocationLink({city, isTabs}: LocationLinkProps):JSX.Element {
   const [searchParams] = useSearchParams();
-  const { [SORT_TYPE]: currentSort, [CITY]: sityQwery } = Object.fromEntries(searchParams);
+  const { [SORT_TYPE]: currentSort, [CITY]: sityQwery } = Object.fromEntries(searchParams) as { [SORT_TYPE]: SortType; [CITY]: Cities };
 
   const isActive = useMemo(() => sityQwery === city, [sityQwery, city]);
 
   return (
     <Link
       className={
-        cn('locations__item-link',
+        cn(
+          'locations__item-link',
           {'tabs__item': isTabs},
           {'tabs__item--active': isActive}
         )
